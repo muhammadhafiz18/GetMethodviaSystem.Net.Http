f2321cf5fc77c6efe4b81f137d591106635c0ade
Muhammad <Muhammad@DESKTOP-SVRE260>
2024-02-03 16:10:41 +0500

    Add project files.

---
+++ firstApplicaton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+public class GetFromServer
+{
+    static async Task Main(string[] args)
+    {
+        using (var client = new HttpClient()) 
+        {
+            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts/10");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"Error: => {response.StatusCode}");
+            }
+
+        }
+    }
+}
